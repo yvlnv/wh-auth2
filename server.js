4bb8b259a270993844e381e569bce5dfd8269b30
yvlnv <ya.volnova@gmail.com>
2021-01-26 20:43:58 +0000

    create basic auth server with sessions and a session store

--- server.js
+++ server.js
@@ -1,53 +1,38 @@
 const express = require('express')
+const session = require('express-session')
 const app = express()
 const {sequelize, User} = require('./models')
+const Sequelize = require("sequelize")
 const bcrypt = require('bcrypt')
+const SequelizeStore = require("connect-session-sequelize")(session.Store)
+
+const sessionSequelize = new Sequelize("database", "username", "password", {
+    dialect: "sqlite",
+    storage: "./sessions.sql",
+  });
 
 app.use(express.json())
 
+const sessionStore = new SequelizeStore({
+    db: sessionSequelize,
+  });
 
-// const basicAuth = require('express-basic-auth')
-// const sqlite3 = require('sqlite3')
-// const db = new sqlite3.Database('db.sql');
+const sessionSettings = {
+    store: myStore,
+    secret: "cats are best",
+    resave: false,
+    saveUninitialized: true
+}
 
-// app.use(basicAuth({
-//     authorizer: dbAuthorizer,
-//     authorizeAsync: true,
-//     challenge: true,
-//     unauthorizedResponse: (req) => {
-//         return `unauthorized. ip: ${req.ip}`
-//     }
-// }));
+sessionStore.sync()
 
-// function dbAuthorizer(username, password, callback) {
-//     const sql = "select password from users where username = ?;";
-//     db.get(sql, [username], async (err, user) => {
-//         err ? callback(err) : bcrypt.compare(password, user.password, callback);
-//     });
-// }
+app.use(session(sessionSettings))
 
 const protect = async (req, res, next) => {
-    const splitted_auth_header = req.headers.authorization.split(" ")[1]
-    const decoded_un_pw = Buffer.from(splitted_auth_header, 'base64').toString()
-    const decoded_un = decoded_un_pw.split(":")[0]
-    const decoded_pw = decoded_un_pw.split(":")[1]
-    const users = await User.findAll({
-        where: {
-            username: decoded_un
-        },
-        limit: 1
-    })
-    if (!users) {
-        res.sendStatus(404)
+    if (!req.session.userId) {
+        res.sendStatus(400)
     } else {
-        const user = users[0]
-        const pw_is_correct = await bcrypt.compare(decoded_pw, user.password)
-        if (!pw_is_correct) {
-            res.sendStatus(400)
-        } else {
-            res.locals.user = user
-            next()
-        }
+        next()
     }
 }
 
@@ -55,12 +40,39 @@ app.get("/", async (req, res) => {
     res.send("Welcome! Sign in here")
 })
 
-app.get("/users/:id", protect, async (req, res) => {
-    if (res.locals.user.id != req.params.id) {
-        res.sendStatus(400)
+app.get("/login", async (req, res) => {
+    const splitted_auth_header = req.headers.authorization.split(" ")[1]
+    const decoded_un_pw = Buffer.from(splitted_auth_header, 'base64').toString()
+    const decoded_un = decoded_un_pw.split(":")[0]
+    const decoded_pw = decoded_un_pw.split(":")[1]
+    const user = await User.findOne({
+        where: {
+            username: decoded_un
+        }
+    })
+    if (!user) {
+        res.sendStatus(404)
     } else {
-        res.send(`Welcome, ${res.locals.user.username}!`)
+        const pw_is_correct = await bcrypt.compare(decoded_pw, user.password)
+        if (!pw_is_correct) {
+            res.sendStatus(400)
+        } else {
+            req.session.userId = user.id
+            res.locals.user = user
+            res.send(`Welcome, ${res.locals.user.username}! You are now logged in`)
+        }
     }
+})
+
+app.get("/logout", async (req, res) => {
+    req.session.destroy()
+    res.send("You are logged out")
+})
+
+app.get("/users/:id", protect, async (req, res) => {
+    const viewer = await User.findByPk(req.session.userId)
+    const viewing = await User.findByPk(req.params.id)
+    res.send(`Welcome, ${viewer.username}! You are looking at ${viewing.username} page`)
 })
 
 app.post("/users", async (req, res) => {
